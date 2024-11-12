dcd5b652e852c836b3ffd80da5dcc09ba138a264
marianotravella <marianotravella2@gmail.com>
2024-11-12 01:31:50 -0300

    Property "Username" added Default value Subscription: "User"

--- Data/Entities/User.cs
+++ Data/Entities/User.cs
@@ -15,9 +15,10 @@ namespace Data.Entities
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserId { get; set; }
         public Subscription Subscription { get; set; }
+        public string Username { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public UserRole Role { get; set; }
+        public UserRole Role { get; set; } = UserRole.User;
     }
 }
