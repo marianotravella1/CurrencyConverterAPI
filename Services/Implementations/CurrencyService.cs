5bc1a67755f480b684737754612f58b628544d29
marianotravella <marianotravella2@gmail.com>
2024-11-11 23:17:08 -0300

    created: Services (Implementations + Interfaces) + Dependency Injections

---
+++ Services/Implementations/CurrencyService.cs
@@ -0,0 +1,19 @@
+﻿using Data.Repository.Interfaces;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Implementations
+{
+    public class CurrencyService : ICurrencyService
+    {
+        private readonly ICurrencyRepository _currencyRepository;
+        public CurrencyService(ICurrencyRepository currencyRepository)
+        {
+            _currencyRepository = currencyRepository;
+        }
+    }
+}
---
+++ Services/Implementations/UserService.cs
@@ -0,0 +1,19 @@
+﻿using Data.Repository.Interfaces;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+    }
+}
