b115509d232aa2febbc81f16b551b0c6d43a4c82
marianotravella <marianotravella2@gmail.com>
2024-11-12 01:34:23 -0300

    created: CurrencyForCreationDTO

---
+++ Common/Models/CurrencyForCreationDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Models
+{
+    public class CurrencyForCreationDTO
+    {
+        public string Code { get; set; }
+        public string Legend { get; set; }
+        public string Symbol { get; set; }
+        public double ConvertibilityIndex { get; set; }
+    }
+}
