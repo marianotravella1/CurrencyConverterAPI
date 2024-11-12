a3a9ce3811895c90925c3569037152563ad39883
marianotravella <marianotravella2@gmail.com>
2024-11-11 23:16:21 -0300

    created: Repositories (Implementations + Interfaces) + Dependency Injections

---
+++ Data/Repository/Implementations/CurrencyRepository.cs
@@ -0,0 +1,20 @@
+﻿using Data.Entities;
+using Data.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository.Implementations
+{
+    public class CurrencyRepository : ICurrencyRepository
+    {
+        private readonly ApplicationContext _context;
+        public CurrencyRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
---
+++ Data/Repository/Implementations/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using Data.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository.Implementations
+{
+    public class UserRepository : IUserRepository
+    {
+        ApplicationContext _context;
+        UserRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
