f8387e888daa7bcb11e356dbd565b276a41b4330
marianotravella <marianotravella2@gmail.com>
2024-11-15 00:21:45 -0300

    Created: ConversionController

---
+++ CurrencyConverterAPI/Controllers/ConversionController.cs
@@ -0,0 +1,38 @@
+﻿using Common.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+using System.Security.Claims;
+
+namespace CurrencyConverterAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConversionController : ControllerBase
+    {
+        private readonly IConversionService _conversionService;
+        public ConversionController(IConversionService conversionService)
+        {
+            _conversionService = conversionService;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetConversionHistory()
+        {
+            int userId = int.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value ?? "");
+            
+            return Ok(_conversionService.GetConversionHistoryByUserId(userId));
+        }
+
+        [HttpPost("CalculateConversion")]
+        [Authorize]
+        public IActionResult CalculateConversion([FromBody] ConversionForCreationDTO calculatorDTO)
+        {
+            int userId = int.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value ?? "");
+
+            return Ok(_conversionService.ConvertCurrency(userId, calculatorDTO));
+        }
+    }
+}
