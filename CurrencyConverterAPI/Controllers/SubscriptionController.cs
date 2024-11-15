4aceb75d611fb4c18aed3b196341134feed5d6c9
marianotravella <marianotravella2@gmail.com>
2024-11-15 00:22:21 -0300

    Created: SubscriptionController

---
+++ CurrencyConverterAPI/Controllers/SubscriptionController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+
+namespace CurrencyConverterAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubscriptionController : ControllerBase
+    {
+        private readonly ISubscriptionService _subscriptionService;
+        public SubscriptionController(ISubscriptionService subscriptionService)
+        {
+            _subscriptionService = subscriptionService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAlSubscriptions()
+        {
+            return Ok(_subscriptionService.GetAllSubscriptions());
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetSubscriptionByName([FromRoute] string name)
+        {
+            return Ok(_subscriptionService.GetSubscriptionByName(name));
+        }
+    }
+}
