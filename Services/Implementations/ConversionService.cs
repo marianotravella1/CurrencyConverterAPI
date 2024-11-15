d511089cc6deb380db057864b0e650e273a78225
marianotravella <marianotravella2@gmail.com>
2024-11-15 00:27:43 -0300

    Created: Cuurency/SubscriptionService

---
+++ Services/Implementations/ConversionService.cs
@@ -0,0 +1,53 @@
+﻿using Common.Models;
+using Data.Entities;
+using Data.Repository.Interfaces;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Implementations
+{
+    public class ConversionService : IConversionService
+    {
+        private readonly IConversionRepository _conversionRepository;
+        private readonly IUserService _userService;
+        private readonly ICurrencyService _currencyService;
+        public ConversionService(IConversionRepository conversionRepository, IUserService userService, ICurrencyService currencyService)
+        {
+            _conversionRepository = conversionRepository;
+            _userService = userService;
+            _currencyService = currencyService;
+        }
+        public IEnumerable<Conversion> GetConversionHistoryByUserId(int userId)
+        {
+            return _conversionRepository.GetConversionHistoryByUserId(userId);
+        }
+        public void AddConversion(int userId, ConversionForCreationDTO conversionDTO)
+        {
+            Conversion newConversion = new Conversion
+            {
+                User = _userService.GetUserById(userId)!,
+                SourceCurrency = _currencyService.GetCurrencyByCode(conversionDTO.SourceCurrencyCode)!,
+                TargetCurrency = _currencyService.GetCurrencyByCode(conversionDTO.TargetCurrencyCode)!,
+                ConvertedAmount = conversionDTO.ConvertedAmount,
+            };
+
+            _conversionRepository.AddConversion(newConversion);
+        }
+        public decimal ConvertCurrency(int userId, ConversionForCreationDTO conversionDTO)
+        {
+            decimal sourceCurrencyCI = _currencyService.GetCurrencyByCode(conversionDTO.SourceCurrencyCode)!.ConvertibilityIndex;
+            decimal targetCurrencyCI = _currencyService.GetCurrencyByCode(conversionDTO.TargetCurrencyCode)!.ConvertibilityIndex;
+            decimal amount = conversionDTO.ConvertedAmount;
+
+            decimal convertedResult = amount * (targetCurrencyCI / sourceCurrencyCI);
+
+            AddConversion(userId, conversionDTO);
+
+            return convertedResult;
+        }
+    }
+}
---
+++ Services/Implementations/SubscriptionService.cs
@@ -0,0 +1,30 @@
+﻿using Common.Models;
+using Data.Entities;
+using Data.Repository.Interfaces;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Implementations
+{
+    public class SubscriptionService : ISubscriptionService
+    {
+        private readonly ISubscriptionRepository _subscriptionRepository;
+        public SubscriptionService(ISubscriptionRepository subscriptionRepository)
+        {
+            _subscriptionRepository = subscriptionRepository;
+        }
+
+        public IEnumerable<SubscriptionsForViewDTO> GetAllSubscriptions()
+        {
+            return _subscriptionRepository.GetAllSubscriptions();
+        }
+        public Subscription? GetSubscriptionByName(string name)
+        {
+            return _subscriptionRepository.GetSubscriptionByName(name);
+        }
+    }
+}
